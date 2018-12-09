756febd85db63b2a69d67dc7f4aea26554860d43
kongxu08 <232090914@qq.com>
2018-12-09 14:29:33 +0800

    允许跨域

--- src/main/java/com/RestTest.java
+++ src/main/java/com/RestTest.java
@@ -3,11 +3,12 @@ package com;
 import com.alibaba.fastjson.JSONObject;
 import com.seeyon.client.CTPRestClient;
 import com.seeyon.client.CTPServiceClientManager;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
+@CrossOrigin
 @RestController
 class RestTest {
     @RequestMapping(value = "/rest2", produces = "application/json;charset=utf-8")
@@ -34,6 +35,14 @@ class RestTest {
         // 取得REST动态客户机实例
         client = clientManager.getRestClient();
         client.authenticate("admin", "cjwsjy123");
+    }
+
+    @RequestMapping(value = "/rest3", produces = "application/json;charset=utf-8")
+    Object rest3() {
+        init();
+        String post = client.get("news/newsType/21?ticket=wangbicheng", String.class);//注意：这里的Map data 切勿传入null，及时data没有信息，也需Map data = new HashMap();
+        Object obj = JSONObject.parse(post);
+        return obj;
     }
 
 }
